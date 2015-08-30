774293e00717c474a7d721e18991e2c085530080
klacs <klacs1200@hotmail.com>
2015-08-30 22:38:33 +0800

    develop

---
+++ program.cs
@@ -0,0 +1,14 @@
+using System.IO;
+using System;
+
+namespace git101;
+{
+	public static class Program
+	{
+		public static Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+	
+}
